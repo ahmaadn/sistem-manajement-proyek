ea2db78df5ed99ef2d59dc3b845ec0da31238cfe
Ahmad Nur <ahmadnursahid12345@gmail.com>
2025-08-17 00:17:09 +0700

    feat: menambahkan method get pada user service

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -2,12 +2,18 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.models.role_model import Role, UserRole
-from app.schemas.user import UserProfile
+from app.schemas.user import UserProfile, UserRead
+from app.services.pegawai_service import PegawaiService
+from app.utils import exceptions
 
 
 class UserService:
-    def __init__(self, session: AsyncSession) -> None:
+    def __init__(
+        self, session: AsyncSession, pegawai_service: PegawaiService
+    ) -> None:
         self.session = session
+
+        self.pegawai_service = pegawai_service
 
     async def get_user_role(self, user_id: int) -> UserRole | None:
         """Mendapatkan role pengguna berdasarkan ID.
@@ -39,3 +45,13 @@ class UserService:
         self.session.add(user_role)
         await self.session.commit()
         return user_role
+
+    async def get(self, user_id: int) -> UserRead | None:
+        user_profile = await self.pegawai_service.get_user_info(user_id)
+
+        if not user_profile:
+            raise exceptions.UserNotFoundError
+
+        user_role = await self.assign_role_to_user(user_id, user_profile)
+
+        return UserRead(**user_profile.model_dump(), role=user_role.role)
