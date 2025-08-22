4291981a31925d9a12d6793e3cf0b031fd24933e
Ahmad Nur <ahmadnursahid12345@gmail.com>
2025-08-22 10:05:12 +0700

    feat: project route di pecah jadi 2 route

--- app/api/api.py
+++ app/api/api.py
@@ -4,6 +4,7 @@ from app.core.config import settings
 
 from .routes import auth_route as auth
 from .routes import dashboard_route as dashboard
+from .routes import project_member_route as project_member
 from .routes import project_route as proyek
 from .routes import task_route as task
 from .routes import user_route as user
@@ -12,5 +13,6 @@ router = APIRouter(prefix=settings.version_url)  # url prefix for all routes ex:
 router.include_router(auth.router)
 router.include_router(user.router)
 router.include_router(proyek.router)
+router.include_router(project_member.router)
 router.include_router(task.router)
 router.include_router(dashboard.router)
--- app/api/routes/project_route.py
+++ app/api/routes/project_route.py
@@ -1,6 +1,6 @@
 from types import NoneType
 
-from fastapi import APIRouter, Body, Depends, Query, status
+from fastapi import APIRouter, Depends, Query, status
 from fastapi_utils.cbv import cbv
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
@@ -9,9 +9,7 @@ from app.api.dependencies.project import get_project_service
 from app.api.dependencies.sessions import get_async_session
 from app.api.dependencies.task import get_task_service
 from app.api.dependencies.user import get_current_user, get_user_service
-from app.db.models.project_member_model import RoleProject
 from app.db.models.project_model import Project
-from app.db.models.role_model import Role
 from app.db.models.task_model import ResourceType, StatusTask
 from app.schemas.pagination import PaginationSchema
 from app.schemas.project import (
@@ -181,132 +179,6 @@ class _Project:
     async def delete_proyek(self, project_id: int) -> NoneType:
         """menghapus proyek"""
         await self.project_service.soft_delete(project_id)
-
-    @r.post(
-        "/projects/{project_id}/members",
-        status_code=status.HTTP_201_CREATED,
-        responses={
-            status.HTTP_201_CREATED: {
-                "description": "Anggota berhasil ditambahkan ke proyek",
-            },
-            status.HTTP_404_NOT_FOUND: {
-                "description": "User tidak ditemukan",
-                "model": exceptions.AppErrorResponse,
-            },
-            status.HTTP_406_NOT_ACCEPTABLE: {
-                "description": "Anggota sudah terdaftar di proyek",
-                "model": exceptions.AppErrorResponse,
-            },
-            status.HTTP_406_NOT_ACCEPTABLE: {
-                "description": (
-                    "Anggota sudah terdaftar di proyek atau Peran tidak valid untuk pengguna. "
-                    "admin hanya bisa menjadi owner dan member tidak dapat diangkat menjadi owner."
-                ),
-                "model": exceptions.AppErrorResponse,
-            },
-        },
-    )
-    async def add_member(
-        self,
-        project_id: int,
-        user_id: int = Body(..., embed=True),
-        role: RoleProject = Body(default=RoleProject.CONTRIBUTOR, embed=True),
-        member: UserService = Depends(get_user_service),
-    ):
-        """menambahkan anggota ke proyek"""
-
-        # validasi project id
-        project_info = await self.project_service.get(project_id)
-        if not project_info:
-            raise exceptions.ProjectNotFoundError
-
-        member_info = await member.get(user_id)
-        if not member_info:
-            raise exceptions.UserNotFoundError
-
-        # admin tidak dapat diangkat selain menjadi owner
-        if (
-            member_info.role in (Role.ADMIN, Role.PROJECT_MANAGER)
-            and role != RoleProject.OWNER
-        ):
-            raise exceptions.InvalidRoleAssignmentError(
-                "admin dan manager hanya bisa menjadi owner."
-            )
-
-        # Member tidak dapat diangkat menjadi owner
-        if member_info.role == Role.TEAM_MEMBER and role == RoleProject.OWNER:
-            raise exceptions.InvalidRoleAssignmentError(
-                "Member tidak dapat diangkat menjadi owner."
-            )
-
-        await self.project_service.add_member(project_id, user_id, role)
-
-        return {"message": "Anggota berhasil ditambahkan ke proyek"}
-
-    @r.delete(
-        "/projects/{project_id}/members/{user_id}",
-        status_code=status.HTTP_202_ACCEPTED,
-        responses={
-            status.HTTP_202_ACCEPTED: {
-                "description": "Anggota berhasil dihapus dari proyek",
-            },
-            status.HTTP_404_NOT_FOUND: {
-                "description": "Anggota tidak ditemukan",
-                "model": exceptions.AppErrorResponse,
-            },
-            status.HTTP_406_NOT_ACCEPTABLE: {
-                "description": "Tidak dapat menghapus anggota proyek",
-                "model": exceptions.AppErrorResponse,
-            },
-        },
-    )
-    async def remove_member(self, user_id: int, project_id: int) -> NoneType:
-        """menghapus anggota dari proyek"""
-
-        # validasi project id
-        project_info = await self.project_service.get(project_id)
-        if not project_info:
-            raise exceptions.ProjectNotFoundError
-
-        member_info = await self.project_service.get_member(project_id, user_id)
-        if not member_info:
-            raise exceptions.MemberNotFoundError
-
-        # tidak bisa menghapus diri sendiri
-        if member_info.user_id == self.user.id:
-            raise exceptions.CannotRemoveMemberError
-
-        await self.project_service.remove_member(project_id, user_id)
-
-    @r.patch(
-        "/projects/{project_id}/members/{user_id}/role",
-        status_code=status.HTTP_202_ACCEPTED,
-        responses={
-            status.HTTP_202_ACCEPTED: {
-                "description": "Peran anggota proyek berhasil diubah",
-            },
-            status.HTTP_404_NOT_FOUND: {
-                "description": "Anggota tidak ditemukan",
-                "model": exceptions.AppErrorResponse,
-            },
-            status.HTTP_406_NOT_ACCEPTABLE: {
-                "description": "Peran tidak valid untuk pengguna",
-                "model": exceptions.AppErrorResponse,
-            },
-        },
-    )
-    async def change_role_project_member(
-        self,
-        project_id: int,
-        user_id: int,
-        role: RoleProject = Body(..., embed=True),
-        user_service: UserService = Depends(get_user_service),
-    ):
-        member_info = await user_service.get(user_id)
-        if not member_info:
-            raise exceptions.MemberNotFoundError
-
-        await self.project_service.change_role_member(project_id, member_info, role)
 
     def _cast_project_to_response(self, proyek: Project) -> ProjectResponse:
         """
